42b9f81d3e3b0e79bcc81c3322ff3367db8898c8
linconlg <linconl_eu@outlook.com>
2022-05-01 15:46:00 -0300

    criado proj.

---
+++ FileManager/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.txtPlanilha = new System.Windows.Forms.TextBox();
+            this.txtArquivo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPastas = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLinhas = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(168, 278);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(82, 27);
+            this.btnExecutar.TabIndex = 5;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // txtPlanilha
+            // 
+            this.txtPlanilha.Location = new System.Drawing.Point(9, 89);
+            this.txtPlanilha.Name = "txtPlanilha";
+            this.txtPlanilha.Size = new System.Drawing.Size(297, 20);
+            this.txtPlanilha.TabIndex = 1;
+            // 
+            // txtArquivo
+            // 
+            this.txtArquivo.Location = new System.Drawing.Point(9, 158);
+            this.txtArquivo.Name = "txtArquivo";
+            this.txtArquivo.Size = new System.Drawing.Size(395, 20);
+            this.txtArquivo.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Caminho da planilha";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Caminho onde estão os arquivos";
+            // 
+            // txtPastas
+            // 
+            this.txtPastas.Location = new System.Drawing.Point(9, 222);
+            this.txtPastas.Name = "txtPastas";
+            this.txtPastas.Size = new System.Drawing.Size(395, 20);
+            this.txtPastas.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Caminho das pastas";
+            // 
+            // txtLinhas
+            // 
+            this.txtLinhas.Location = new System.Drawing.Point(327, 88);
+            this.txtLinhas.Name = "txtLinhas";
+            this.txtLinhas.Size = new System.Drawing.Size(77, 20);
+            this.txtLinhas.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(327, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Linhas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 372);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLinhas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPastas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtArquivo);
+            this.Controls.Add(this.txtPlanilha);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.TextBox txtPlanilha;
+        private System.Windows.Forms.TextBox txtArquivo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPastas;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLinhas;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FileManager/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int linhas = Convert.ToInt32(txtLinhas.Text);
+                string diretorioExcel = txtPlanilha.Text;
+                DirectoryInfo pastaArquivos = new DirectoryInfo($@"{txtArquivo.Text}");
+                DirectoryInfo pastaAlvo = new DirectoryInfo($@"{txtPastas.Text}");
+
+                FileInfo[] listaArquivos = pastaArquivos.GetFiles();
+                DirectoryInfo[] listaPastas = pastaAlvo.GetDirectories();
+                DirectoryInfo pastaAtual;
+                FileInfo[] arquivosPasta;
+
+                string[] nomesArquivos = new string[linhas];
+                string[] pastas = new string[linhas];
+
+
+                //Recebe as informações da planilha
+                var planilha = new Microsoft.Office.Interop.Excel.Application();
+                var wb = planilha.Workbooks.Open($@"{diretorioExcel}", ReadOnly: true);
+                var ws = wb.Worksheets[1];
+                var r = ws.Range["A1"].Resize[linhas, 2];
+                var array = r.Value;
+
+                for (int i = 1; i <= linhas; i++)
+                {
+                    for (int j = 1; j <= 2; j++)
+                    {
+                        string text = Convert.ToString(array[i, j]);
+
+                        if (j == 1)
+                        {
+                            nomesArquivos[i - 1] = text;
+                        }
+                        else
+                        {
+                            pastas[i - 1] = text;
+                        }
+                    }
+                }
+
+                //-------------------------------------------------------------
+
+                for (int i = 0; i < linhas; i++)
+                {
+                    if (Directory.Exists($@"{txtPastas.Text}\{pastas[i]}"))
+                    {
+
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory($@"{txtPastas.Text}\{pastas[i]}");
+                    }
+                }
+
+
+
+                foreach (FileInfo arquivo in listaArquivos)
+                {
+                    if (Path.GetFileName(arquivo.FullName) == "desktop.ini")
+                    {
+
+                    }
+                    else
+                    {
+                        string extensao = Path.GetExtension(arquivo.FullName).ToLower();
+                        int indRev = Path.GetFileNameWithoutExtension(arquivo.FullName).LastIndexOf(" Rev");
+                        string nomeArquivo = Path.GetFileNameWithoutExtension(arquivo.FullName).Substring(0, indRev);
+
+                        for (int i = 1; i <= linhas; i++)
+                        {
+                            if (nomesArquivos[i - 1] == nomeArquivo)
+                            {
+                                pastaAtual = new DirectoryInfo($@"{txtPastas.Text}\{pastas[i - 1]}");
+                                arquivosPasta = pastaAtual.GetFiles();
+
+                                foreach (FileInfo item in arquivosPasta)
+                                {
+                                    if (Path.GetFileName(item.FullName) != "desktop.ini")
+                                    {
+                                        int indrev = Path.GetFileNameWithoutExtension(item.FullName).LastIndexOf(" Rev");
+
+                                        if (Path.GetFileNameWithoutExtension(item.FullName).Substring(0, indrev) == Path.GetFileNameWithoutExtension(arquivo.FullName).Substring(0, indRev))
+                                        {
+                                            File.Move(item.FullName, $@"{Path.GetDirectoryName(item.FullName)}\Superado\{Path.GetFileName(item.FullName)}");
+                                        }
+                                    }
+
+                                }
+
+                                if (File.Exists($@"{txtPastas.Text}\{pastas[i - 1]}\{Path.GetFileName(arquivo.FullName)}"))
+                                {
+                                    File.Delete($@"{txtPastas.Text}\{pastas[i - 1]}\{Path.GetFileName(arquivo.FullName)}");
+                                }
+
+                                File.Move(arquivo.FullName, $@"{txtPastas.Text}\{pastas[i - 1]}\{Path.GetFileName(arquivo.FullName)}");
+
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                MessageBox.Show("Processo concluído");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
